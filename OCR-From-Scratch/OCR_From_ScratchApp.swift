21e90a564d394f7884cbf1ad496551008f92d258
bair0069 <bair0069@algonquinlive.com>
2023-01-27 15:43:20 -0500

    Initial Commit

---
+++ OCR-From-Scratch/OCR_From_ScratchApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCR_From_ScratchApp.swift
+//  OCR-From-Scratch
+//
+//  Created by Kiefer Baird-Perry on 2023-01-27.
+//
+
+import SwiftUI
+
+@main
+struct OCR_From_ScratchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
