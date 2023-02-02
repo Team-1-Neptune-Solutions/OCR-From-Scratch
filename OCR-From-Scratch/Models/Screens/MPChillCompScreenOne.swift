c8a5fe400a6e4960533d7c020eaf966c2d53be57
Will Paceley <pace0033@algonquinlive.com>
2023-02-01 19:07:28 -0500

    add parse function scaffold for first screen

--- OCR-From-Scratch/Models/Screens/MPChillCompScreenOne.swift
+++ OCR-From-Scratch/Models/Screens/MPChillCompScreenOne.swift
@@ -11,14 +11,46 @@ struct MPChillCompScreenOne: ParsableScreen {
     var id = UUID()
     var description = "Main Plant Chiller Compressor Page 1"
     
+    let labels = [
+        "Compressor Starts",
+        "Running Time",
+        "Oil Tank Discharge Pressure",
+        "Oil Differential Pressure"
+    ]
+    
+    // WP Note - We need these values:
+    // Compressor Starts
+    // Running Time
+    // Oil Tank Discharge Pressure
+    // Oil Differential Pressure
+    
     func parse(results: [String]) -> String {
+        var myValues = [String]()
+        var ourFloats = [Float]()
+        
         var parsedResults = ""
         print("--- PRINTING RESULTS ---")
-        for result in results {
+        for (index, result) in results.enumerated() {
+            if result.contains("Min") {
+                myValues.append(result)
+            } else if result.contains("PSIG") {
+                let splitResult = result.split(separator: " ")
+                if let ourNum = Float(splitResult[0]) {
+                    if ourNum > 0 {
+                        ourFloats.append(ourNum)
+                    }
+                }
+                myValues.append(result)
+            } else if result.contains("PSID") {
+                myValues.append(result)
+            }
+            
             parsedResults += "\(result)\n"
         }
         
         print(parsedResults)
+        print(myValues)
+        print(ourFloats)
         return parsedResults
     }
 }
