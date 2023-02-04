95369429b75900baddf1214ab1e4957842b3ba4a
Akshay Mahajan <maha0134@algonquinlive.com>
2023-02-04 11:32:16 -0500

    list id issue resolved

---
+++ OCR-From-Scratch/Models/Screens/AbsorptionChiller.swift
@@ -0,0 +1,26 @@
+//
+//  AbsorptionChiller.swift
+//  OCR-From-Scratch
+//
+//  Created by AKSHAY MAHAJAN on 2023-02-04.
+//
+
+import Foundation
+
+struct AbsorptionChiller: ParsableScreen {
+    let id = UUID().uuidString
+    let description = "Absorption Chiller"
+    
+    func parse(results: [String]) -> String {
+        var values = [Double]()
+        var parsedResults = ""
+        print("--- PRINTING RESULTS ---")
+        for result in results {
+            if let value = Double(result){
+                values.append(value)
+                parsedResults += "\(result)\n"
+            }
+        }
+        return parsedResults
+    }
+}
